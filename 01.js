5801051eb93eaa1f5b73203145f51ab558a8293a
Jamer Rq <jamerrq@gmail.com>
2023-03-09 23:50:17 -0500

    added everything

---
+++ 01.js
@@ -0,0 +1,25 @@
+/*
+  Importante:
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas.
+  No comentar la funcion
+*/
+function soloNumeros(array) {
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // Debe devolver un arreglo con solo los enteros.
+  // Ej:
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+
+  // Tu código aca:
+  var nuevo_arreglo = [];
+  for(var i = 0; i < array.length; ++i){
+    var element = array[i];
+    if(typeof element == 'number')nuevo_arreglo.push(element);
+  }
+  return nuevo_arreglo;
+}
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = soloNumeros
